1bcd9f5fd72f37113c072c71dacb32cc0a1d1125
Jasper <bink.neele@gmail.com>
2024-08-15 15:54:48 +0200

    made main game loop canged player move

--- src/games/tic_tac_to.rs
+++ src/games/tic_tac_to.rs
@@ -1,3 +1,5 @@
+use super::usfol::user_input;
+
 
 fn print_map(map: Vec<Vec<char>>){
 
@@ -9,7 +11,7 @@ fn print_map(map: Vec<Vec<char>>){
     }
 }
 
-fn win_chack(map: Vec<Vec<char>> ) -> bool{
+fn win_chack(map: Vec<Vec<char>> ) -> (bool, char){
     let mut streak = 0;
     let mut player = 'x';
     for i in 0..3{
@@ -23,7 +25,7 @@ fn win_chack(map: Vec<Vec<char>> ) -> bool{
                 }
             } //return true if won
             if streak == 3{
-                return true;
+                return (true, player);
             }
             //reset valuse
             streak = 0;
@@ -36,17 +38,17 @@ fn win_chack(map: Vec<Vec<char>> ) -> bool{
                 }
             } //return true if won
             if streak == 3{
-                return true;
+                return (true, player);
             }
             //reset valuse
             streak = 0;
 
             //thack vurtekole lines
             if map[0 * 2 + 2][0 * 2 + 2] == player && map[1 * 2 + 2][1 * 2 + 2] == player && map[2 * 2 + 2][2 * 2 + 2] == player {
-                return true;
+                return (true, player);
             }
             if map[0 * 2 + 2][2 * 2 + 2] == player && map[1 * 2 + 2][1 * 2 + 2] == player && map[2 * 2 + 2][0 * 2 + 2] == player {
-                return true;
+                return (true, player);
             }
             //reset valuse
             streak = 0;
@@ -55,39 +57,68 @@ fn win_chack(map: Vec<Vec<char>> ) -> bool{
         }
         player = 'x';
     }
-    return false;
+    return (false, ' ');
 }
 
 fn player_move(mut map: Vec<Vec<char>>, input: String, player: char) -> Vec<Vec<char>> {
     let split_input: Vec<char> = input.chars().collect();
     let row_charr = split_input[0];
-    let mut row = 0;
-    let colum = split_input[1];
-    if row_charr == 'a'{row = 0;}
-    else if row_charr == 'c'{row = 1;}
-    else if row_charr == 'd'{row = 2;}
-    else {println!("not a valid char"); return map;}
+    let colum_charr = split_input[1];
 
-    map[colum as usize * 2 + 2 as usize][row * 2 + 2 as usize] = player;
+    let row = match row_charr {
+        'A'|'a' => 2,
+        'B'|'b' => 4,
+        'C'|'c' => 6,
+        _ => {println!("Not a valid char"); return map;}
+    };
+
+    let col = match colum_charr {
+        '1' => 2,
+        '2' => 4,
+        '3' => 6,
+        _ => {println!("Not a valid number"); return map;}
+    };
+
+    if map[row][col] == ' ' {  // Ensure the spot is empty
+        map[row][col] = player;
+    } else {
+        println!("Spot already taken!");
+    }
     return map;
 }
 
 pub fn start(){
     // to only exes the game tiles use this i*2+3, i = the index 0..2
-    let map: Vec<Vec<char>> = vec![vec![' ', '|', '1', '|', '2', '|', '3', '|', ' '],
+    let mut map: Vec<Vec<char>> = vec![vec![' ', '|', '1', '|', '2', '|', '3', '|', ' '],
                                    vec!['—', '+', '—', '—', '—', '—', '—', '+', '—'],
-                                   vec!['A', '|', 'o', '|', ' ', '|', 'x', '|', 'A'],
+                                   vec!['A', '|', ' ', '|', ' ', '|', ' ', '|', 'A'],
                                    vec!['—', '|', '—', '+', '—', '+', '—', '|', '—'],
-                                   vec!['B', '|', ' ', '|', 'o', '|', ' ', '|', 'B'],
+                                   vec!['B', '|', ' ', '|', ' ', '|', ' ', '|', 'B'],
                                    vec!['—', '|', '—', '+', '—', '+', '—', '|', '—'],
-                                   vec!['C', '|', 'o', '|', ' ', '|', 'o', '|', 'C'],
+                                   vec!['C', '|', ' ', '|', ' ', '|', ' ', '|', 'C'],
                                    vec!['—', '+', '—', '—', '—', '—', '—', '+', '—'],
                                    vec![' ', '|', '1', '|', '2', '|', '3', '|', ' ']];
 
 
     let player1 = 'x';
     let player2 = 'o';
-    let turn = 1;
-    let runing = true;
+    let mut turn = 1;
+    let mut runing = true;
+
+    while runing {
+        print_map(map.clone());
+        if turn == 1{
+            map = player_move(map, user_input(""), player1);
+            turn +=1;
+        } else{
+            map = player_move(map.clone(), user_input(""), player2);
+            turn = 1;
+        }
+        let (is_doune, winer) = win_chack(map.clone());
+        if is_doune {
+            runing = false;
+            println!("player whit the charector {}  hase wone", winer);
+        }
+    }
 
 }
