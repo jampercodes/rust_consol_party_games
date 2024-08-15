d4d3c49d05fec6a7b0b060abaff5f5b94801e831
jasper <bink.neele@gmail.com>
2024-08-15 10:26:38 +0200

    made a user input function

--- src/games/usfol.rs
+++ src/games/usfol.rs
@@ -1,5 +1,25 @@
 
 
-pub fn user_input() -> String{
-    return String::new();
+pub fn user_input(Print: &str) -> String{
+    
+    let mut user_input_string =  String::new();
+
+    print!("{}", Print);
+    //--------USER INPUT--------\\
+
+    // fluse input bufer
+    let _=stdout().flush();
+
+    // user input
+    stdin().read_line(&mut user_input_string).expect("Did not enter a correct string");
+
+    // remove newline carektors from input
+    if let Some('\n')=user_input_string.chars().next_back() {
+        user_input_string.pop();
+    }
+    if let Some('\r')=user_input_string.chars().next_back() {
+        user_input_string.pop();
+    }
+
+    return user_input_string; 
 }
